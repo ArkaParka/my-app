381b03c461b57bfe22d48aa771726fa0959216a4
Jever Osmanova <amidala274@gmail.com>
2020-04-09 18:20:10 +0300

    Button and form added

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, HostListener, ViewChild } from '@angular/core';
 import { DynamicMenuService } from './dynamic-menu.service';
 import { GridOptions } from 'ag-grid-community';
+import { FormGroup } from '@angular/forms';
+import { ModalDirective } from 'ngx-bootstrap';
+import { FormlyFieldConfig } from '@ngx-formly/core';
 
 @Component({
   templateUrl: './menu.component.html'
@@ -11,22 +14,51 @@ export class MenuComponent implements OnInit {
   public gridOptions: GridOptions;
   public configData;
   public rowData: any;
+  public templateCtx = {};
+  public fields: FormlyFieldConfig[];
+  public form = new FormGroup({});
+  public model: any = {};
+  public gridApi;
 
   constructor(private callRequestForGetModuleConfig: DynamicMenuService) {
     console.log("Тест");  
   }
 
-  ngOnInit () {
+  @ViewChild('largeModal') public largeModal: ModalDirective;
+
+  @HostListener ('click', ['$event']) onClick(e: MouseEvent) {
+    if (e.target['value'] == this.templateCtx['value']) {
+      this.largeModal.show();
+      console.log("Нажатие мышки", e.target['value']);
+    }
+  }
+
+  ngOnInit (): void {
     this.workWithConfig();
   }
 
-  public workWithConfig() {
+  public workWithConfig(): void {
     this.callRequestForGetModuleConfig.getModuleMenuFormConfig("test", "test").subscribe(resp => {
-      this.configData = resp.dataFromViewConfig;
-      //console.log("Тест answer data", this.configData.columnDefs);
+      this.gridOptions = resp.dataFromViewConfig;
+      this.fields = resp.dataFromDataTypes;
+      this.templateCtx = {
+        value: resp.dataFromActions.actionName,
+        title: resp.dataFromActions.actionTitle
+      }
     });
     this.rowData = this.callRequestForGetModuleConfig.getModuleData("test", "test", 10);
-    this.gridOptions = this.configData;
+  }
+
+  public submit(): void {
+    alert(JSON.stringify(this.model));
+  }
+
+  public hideForm(): void {
+    this.largeModal.hide();
+  }
+
+  public add(): void {
+    this.largeModal.hide();
   }
 
 }
--- src/app/menu/menu.module.ts
+++ src/app/menu/menu.module.ts
@@ -5,9 +5,11 @@ import { NgModule } from '@angular/core';
 import { MenuRoutingModule } from './menu-routing.module';
 import { MenuComponent } from './menu.component';
 import { AgGridModule } from 'ag-grid-angular';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
+    SharedModule,
     CommonModule,
     MenuRoutingModule,
     AgGridModule.withComponents([])
