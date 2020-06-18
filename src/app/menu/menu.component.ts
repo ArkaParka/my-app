994681cf3120c5ca91cd3b2ab2925e43217346a0
Jever Osmanova <amidala274@gmail.com>
2020-06-18 15:10:45 +0300

    Таблица ng-zorro

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit, HostListener, ViewChild, ElementRef } from '@angular/core';
 import { DynamicMenuService } from '../services/dynamic-menu.service';
-import { GridOptions } from 'ag-grid-community';
+import { GridOptions, IDatasource, IGetRowsParams, Module } from 'ag-grid-community';
 import { FormGroup } from '@angular/forms';
 import {ModalDirective} from "ngx-bootstrap/modal";
 import { FormlyFieldConfig, FormlyFormOptions } from '@ngx-formly/core';
 import { Actions } from '../models/Actions.interface';
 import { DataTypes } from '../models/DataTypes.interface';
-import { Subject, BehaviorSubject } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
+import { NzTableQueryParams } from 'ng-zorro-antd/table/ng-zorro-antd-table';
+//import { InfiniteRowModelModule } from '@ag-grid-community/infinite-row-model'
 
 @Component({
   templateUrl: './menu.component.html',
@@ -29,32 +30,135 @@ export class MenuComponent implements OnInit {
   public putFormData: object = {};
   private hash: string = null;
   private idFieldName = null;
-  private id: number;
+  private id: string;
   private bodyForRequest;
   public confirmMessage;
   private typeForm;
   public viewConfig;
-  private gridApi: any;
+  // private gridApi: any;
   public options: FormlyFormOptions = {};
-  public deleteIndicator;
+  // public deleteIndicator;
   public REQ_ONE;
   public REQ_MULTY;
+  private one_id: string = '';
+  private multy_id: string[] = [];
   public NO_REQ = null;
-  private currentPage;
-  private getPageSize;
-  private sortModel;
-  public listOfPageSize = [10, 20, 30, 40, 50];
+  // public listOfPageSize = [10, 20, 30, 40, 50];
+  // private currentPage = 0;
+  // private getPageSize = this.listOfPageSize[0];
+  // private sortModel;
+  //dataSource: IDatasource; 
+  //public modules: Module[] = [InfiniteRowModelModule];
 
+  total = 1;
+  listOfModuleData: object[] = [];
+  loading = true;
+  pageSize = 10;
+  pageIndex = 1;
+  checked = false;
+  indeterminate = false;
+  listOfCurrentPageData: object[] = [];
+  listOfData: object[] = [];
+  setOfCheckedId = new Set<string>();
+
+  listOfSelection = [
+    {
+      text: 'Select All Row',
+      onSelect: () => {
+        this.onAllChecked(true);
+      }
+    },
+    {
+      text: 'Select Odd Row',
+      onSelect: () => {
+        this.listOfCurrentPageData.forEach((data, index) => this.updateCheckedSet(data[this.idFieldName], index % 2 !== 0));
+        this.refreshCheckedStatus();
+      }
+    },
+    {
+      text: 'Select Even Row',
+      onSelect: () => {
+        this.listOfCurrentPageData.forEach((data, index) => this.updateCheckedSet(data[this.idFieldName], index % 2 === 0));
+        this.refreshCheckedStatus();
+      }
+    }
+  ];
+
+  updateCheckedSet(login: string, checked: boolean): void {
+    if (checked) {
+      this.setOfCheckedId.add(login);
+      this.REQ_ONE = false;
+      this.REQ_MULTY = true;
+      this.multy_id.push(login);
+      this.oneIdTemplate(this.setOfCheckedId.size, login);
+    } else {
+      this.setOfCheckedId.delete(login);
+      this.multy_id.splice( this.multy_id.indexOf(login), 1);
+      this.oneIdTemplate(this.setOfCheckedId.size, login);
+    }
+    if (this.setOfCheckedId.size == 0) {
+      this.REQ_ONE = false;
+      this.REQ_MULTY = false;
+    } 
+  }
+
+  oneIdTemplate (size, login) {
+    if (size == 1) {
+      this.REQ_ONE = true;
+      this.one_id = login;
+    }
+  }
+
+  onItemChecked(login: string, checked: boolean): void {
+    this.updateCheckedSet(login, checked);
+    this.refreshCheckedStatus();
+  }
+
+  onAllChecked(value: boolean): void {
+    this.listOfCurrentPageData.forEach(item => this.updateCheckedSet(item[this.idFieldName], value));
+    this.refreshCheckedStatus();
+  }
+
+  onCurrentPageDataChange($event: object[]): void {
+    this.listOfCurrentPageData = $event;
+    this.refreshCheckedStatus();
+  }
+
+  refreshCheckedStatus(): void {
+    this.checked = this.listOfCurrentPageData.every(item => this.setOfCheckedId.has(item[this.idFieldName]));
+    this.indeterminate = this.listOfCurrentPageData.some(item => this.setOfCheckedId.has(item[this.idFieldName])) && !this.checked;
+  }
 
   constructor(private dynamicMenuService: DynamicMenuService, private route: ActivatedRoute) {
     route.params.subscribe((params) => {
       this.moduleKey = params['moduleKey'];
       this.configPath = params['configPath'];
     });
+    // this.dataSource = {
+    //   getRows: (params: IGetRowsParams) => {
+    //     const bodyForGetModuleData = {
+    //       action_name: this.configPath,
+    //       order_info: [
+    //         this.sortModel
+    //       ],
+    //       page_info: {
+    //         pageIndex: this.currentPage,
+    //         pageSize: this.getPageSize
+    //       }
+    //     };
+    //     this.dynamicMenuService.getModuleData(this.moduleKey, bodyForGetModuleData).subscribe(resp => {
+    //       params.successCallback(resp.data, Number(resp.total_size));
+    //       // this.gridOptions.api.setRowData(resp.data);
+    //       // this.gridApi.refreshCells({force: true});
+    //     });
+    //   }
+    // };  
+
+
   }
 
   @ViewChild('largeModal') public largeModal: ModalDirective;
-  @ViewChild('pageSize') public pageSize: ElementRef;
+  //@ViewChild('pageSize') public pageSize: ElementRef;
 
   @HostListener ('click', ['$event']) onClick(e: MouseEvent) {
     let forms;
@@ -94,7 +198,7 @@ export class MenuComponent implements OnInit {
         id: this.id, 
         type: this.typeForm
       };
-      this.idFieldName = this.viewConfig.config.idFieldName;
+
       if (e.target['value']?.includes('edit')) {
         this.getFormDataInstance(this.typeForm);
       } 
@@ -105,7 +209,8 @@ export class MenuComponent implements OnInit {
 
   ngOnInit(): void {
     this.workWithConfig();
-    this.addData();
+    
+    //this.addData(this.pageIndex, this.pageSize, null, null);
   }
 
   disableFunc(type: string): boolean {
@@ -130,6 +235,7 @@ export class MenuComponent implements OnInit {
       this.dataTypes = resp.dataTypes;
       this.actions = resp.actions;
       this.gridOptions = resp.viewConfig.config;
+      this.idFieldName = this.viewConfig.config.idFieldName;
     });
     const testModel = {
       phoneInfos: [
@@ -140,24 +246,35 @@ export class MenuComponent implements OnInit {
     this.model = testModel;
   }
 
-  public addData(): void {
+  public addData(
+    pageIndex: number,
+    pageSize: number,
+    sortField: string | null,
+    sortOrder: string | null
+    ): void {
     const bodyForGetModuleData = {
       action_name: this.configPath,
       order_info: [
-        this.sortModel
+        {
+          field_path: sortField,
+          order: sortOrder
+        }
       ],
       page_info: {
-        pageIndex: this.currentPage,
-        pageSize: this.getPageSize
+        pageIndex: pageIndex,
+        pageSize: pageSize
       }
     };
-    this.dynamicMenuService.getModuleData( this.moduleKey, bodyForGetModuleData).subscribe(data => {
-      this.rowData = data.data;
+    this.loading = true;
+    this.dynamicMenuService.getModuleData(this.moduleKey, bodyForGetModuleData).subscribe(data => {
+      //this.rowData = data.data;
+      this.loading = false;
+      this.total =  data.total_size; 
+      this.listOfModuleData = data.data;
     });
   }
 
-  submit() {
-  }
+  submit() { }
 
   public hideForm(): void {
     this.form.reset();
@@ -174,8 +291,8 @@ export class MenuComponent implements OnInit {
       this.putFormDataInstance();
     }
     this.form.reset();
-    this.addData();
-    this.gridApi.refreshCells({force : true});
+    //this.addData();
+    //this.gridApi.refreshCells({force : true});
     this.largeModal.hide();
   }
 
@@ -185,12 +302,11 @@ export class MenuComponent implements OnInit {
       delete  this.bodyForRequest.id;
     }
 
-    this.dynamicMenuService.putFormDataInstance( this.moduleKey, this.bodyForRequest).subscribe();       
+    this.dynamicMenuService.putFormDataInstance(this.moduleKey, this.bodyForRequest).subscribe();       
   }
 
   private getFormDataInstance(typeForm: string): void {
-    this.id = this.REQ_ONE[this.idFieldName];
-    this.dynamicMenuService.getFormDataInstance( this.moduleKey, (this.putFormData as any).formKey, typeForm, this.id).subscribe(data => {
+    this.dynamicMenuService.getFormDataInstance( this.moduleKey, (this.putFormData as any).formKey, typeForm, this.one_id).subscribe(data => {
       this.model = data.data;
       this.hash = data.hash;
       this.id = data.id;
@@ -201,38 +317,50 @@ export class MenuComponent implements OnInit {
   }
 
   private deleteFormDataInstance(typeForm: string): void {
-    this.REQ_MULTY.map(elem => {
-      this.id = elem[this.idFieldName];
-      this.dynamicMenuService.deleteFormDataInstance( this.moduleKey, (this.putFormData as any).formKey, typeForm, this.id).subscribe();
-    });
-    
-  }
-
-  onGridReady(params) {
-    this.gridApi = params.api;
-    this.gridApi.sizeColumnsToFit();
-    this.gridApi.paginationSetPageSize(this.listOfPageSize[0]);
-  }
-
-  rowClicked(event) {
-    this.REQ_ONE = event.data;
-    this.REQ_MULTY = this.gridApi.getSelectedRows();
-    if (this.REQ_MULTY.length > 1) {
-      this.REQ_ONE = null;  
-    }
-    this.currentPage = this.gridOptions.api.paginationGetCurrentPage();
-    this.getPageSize = this.gridOptions.api.paginationGetPageSize();
-    console.log('Размер страницы', this.gridOptions.api.paginationGetPageSize());
-    this.sortModel = this.gridOptions.api.getSortModel().map(elem => {
-      return {
-        field_path: elem.colId ? elem.colId : null,
-        order: elem.sort ? elem.sort : null
-      }
+    this.multy_id.map(elem => {
+      this.dynamicMenuService.deleteFormDataInstance( this.moduleKey, (this.putFormData as any).formKey, typeForm, elem).subscribe();
     });
   }
 
-  setPageSize($event) {
-    this.gridApi.paginationSetPageSize($event.target.value);
+  onQueryParamsChange(params: NzTableQueryParams): void {
+    console.log(params);
+    const { pageSize, pageIndex, sort, filter } = params;
+    const currentSort = sort.find(item => item.value !== null);
+    const sortField = (currentSort && currentSort.key) || null;
+    const sortOrder = (currentSort && currentSort.value) || null;
+    this.addData(pageIndex, pageSize, sortField, sortOrder);
   }
+
+  // onGridReady(params) {
+  //   this.gridApi = params.api;
+  //   this.gridOptions.api.setColumnDefs(this.gridOptions.columnDefs);
+  //   this.gridOptions.rowModelType = 'infinite'; 
+  //   this.gridOptions.cacheBlockSize = 10;
+  //   this.gridApi.paginationSetPageSize(this.listOfPageSize[0]);
+  //   this.gridApi.sizeColumnsToFit();
+  //   //this.gridApi.setDatasource(this.dataSource);
+  //   this.addData();
+  // }
+
+  // rowClicked(event) {
+  //   this.REQ_ONE = event.data;
+  //   this.REQ_MULTY = this.gridApi.getSelectedRows();
+  //   if (this.REQ_MULTY.length > 1) {
+  //     this.REQ_ONE = null;  
+  //   }
+  //   this.currentPage = this.gridOptions.api.paginationGetCurrentPage();
+  //   this.getPageSize = this.gridOptions.api.paginationGetPageSize();
+  //   this.sortModel = this.gridOptions.api.getSortModel().map(elem => {
+  //     return {
+  //       field_path: elem.colId ? elem.colId : null,
+  //       order: elem.sort ? elem.sort : null
+  //     }
+  //   });
+  // }
+
+  // setPageSize($event) {
+  //   this.gridApi.paginationSetPageSize(Number($event.target.value));
+  //   //this.addData();
+  // }
 
 }
--- src/app/menu/menu.module.ts
+++ src/app/menu/menu.module.ts
@@ -1,4 +1,3 @@
-// Angular
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
@@ -6,16 +5,21 @@ import { MenuRoutingModule } from './menu-routing.module';
 import { MenuComponent } from './menu.component';
 import { AgGridModule } from 'ag-grid-angular';
 import { SharedModule } from '../shared/shared.module';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
+import { NzTableModule } from 'ng-zorro-antd/table';
 
 @NgModule({
   imports: [
     SharedModule,
     CommonModule,
     MenuRoutingModule,
-    AgGridModule.withComponents([])
+    AgGridModule.withComponents([]),
+    NzTableModule,
+    ScrollingModule,
+    DragDropModule
   ],
-  declarations: [
-    MenuComponent,
-  ]
+  declarations: [ MenuComponent ]
 })
 export class MenuModule { }
