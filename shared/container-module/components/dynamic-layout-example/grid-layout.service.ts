6f33bc5363461d0bbcff6a3efa2c1342d6fdcdb4
Jever Osmanova <amidala274@gmail.com>
2020-07-02 13:36:13 +0300

    Запрос списка компонентов на сервер

--- shared/container-module/components/dynamic-layout-example/grid-layout.service.ts
+++ shared/container-module/components/dynamic-layout-example/grid-layout.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {Observable, of} from "rxjs";
 import {DynamicLayoutConfig} from "../../../../src/app/models/DynamicLayoutConfig";
-import {layoutConfig} from "./newGridConfigMock";
+import {layoutConfig, headerConfig} from "./newGridConfigMock";
 
 @Injectable({
   providedIn: 'root'
@@ -15,8 +15,13 @@ export class GridLayoutService {
     return of(layoutConfig);
   }
 
-  getGridAreaConfiguration(areaName: string): Observable<any> {
-    return of();
+  getGridAreaConfiguration(areaName:string): Observable<any> {
+
+    switch(areaName) {
+      case'main': return of(['header']);
+      default: return of([])
+    }
+    
   }
 
 }
