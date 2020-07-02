e9dceb342057c724ad5badc3f76e55fe1b9c7ba8
Jever Osmanova <amidala274@gmail.com>
2020-07-02 13:42:59 +0300

    Динамический загрузчик компонентов

---
+++ shared/container-module/components/grid-item/dc-layout.ts
@@ -0,0 +1,44 @@
+import { Component, Input, OnInit, ViewChild, ComponentFactoryResolver, Injector } from '@angular/core';
+import { DynamicComponentDirective } from '../grid-item-content/dynamic-component-directive';
+import { getComponent } from '../avaiableComponents/list-components';
+
+
+@Component({
+  selector: 'dc-component',
+  template: `
+              <div>
+                <h1>TEST</h1>
+                <ng-template dynamic-component></ng-template>
+              </div>
+            `
+})
+export class DClayoutComponent implements OnInit {
+  @Input('dynamicLayout') dynamicLayout;
+  @ViewChild(DynamicComponentDirective, {static: true}) dynamicComponent: DynamicComponentDirective;
+  
+
+  constructor(private componentFactoryResolver: ComponentFactoryResolver,
+              private injector: Injector) { }
+
+  ngOnInit() {
+    console.log(this.dynamicLayout)
+    this.loadComponent();
+  }
+
+  loadComponent() {
+    const component = getComponent(this.dynamicLayout);
+    const componentFactory = this.componentFactoryResolver.resolveComponentFactory(component.component);
+    const viewContainerRef = this.dynamicComponent.viewContainerRef;
+    viewContainerRef.clear();
+    const injector =  this.createInjector(component.services);
+    viewContainerRef.createComponent(componentFactory, 0, injector);
+  }
+
+  createInjector(services){
+    return Injector.create({
+      parent: this.injector,
+      providers: services
+    });
+  }
+
+}
