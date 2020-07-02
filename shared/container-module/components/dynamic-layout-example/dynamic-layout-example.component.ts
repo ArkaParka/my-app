93891bcc7e08cbf620abe2de2462dbed593a1d44
Jever Osmanova <amidala274@gmail.com>
2020-07-02 13:35:21 +0300

    +++

--- shared/container-module/components/dynamic-layout-example/dynamic-layout-example.component.ts
+++ shared/container-module/components/dynamic-layout-example/dynamic-layout-example.component.ts
@@ -1,8 +1,7 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild } from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {DynamicLayoutConfig} from "../../../../src/app/models/DynamicLayoutConfig";
 import {GridLayoutService} from "./grid-layout.service";
-import {zip} from "rxjs";
 
 @Component({
   selector: 'app-form-loader',
@@ -10,16 +9,16 @@ import {zip} from "rxjs";
   styleUrls: ['./dynamic-layout-example.component.scss']
 })
 export class DynamicLayoutExampleComponent {
-  gridTemplate: DynamicLayoutConfig;
 
+  gridTemplate: DynamicLayoutConfig;
   isLoading: boolean = true;
+ 
 
   constructor(private route: ActivatedRoute, private gridLayoutService: GridLayoutService) {
     this.gridLayoutService.getGridConfiguration().subscribe(result => {
       this.gridTemplate = result;
       this.isLoading = false;
-    })
-  }
-
+    });
+  }  
 
 }
