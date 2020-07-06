b671f66aba36739ce424c7dd8f591f36509d8995
andreykichatoff <andreykichatoff@gmail.com>
2020-07-06 17:39:03 +0400

    Визуальная отметка выбранного элемента в дереве и его родителей

--- src/app/containers/default-layout/sidebar-navigation/sidebar-navigation.component.ts
+++ src/app/containers/default-layout/sidebar-navigation/sidebar-navigation.component.ts
@@ -1,6 +1,6 @@
-import {Component, HostBinding, Input, ViewEncapsulation} from "@angular/core";
+import {Component, HostBinding, Input, ViewChild, ViewEncapsulation} from "@angular/core";
 import {Router} from "@angular/router";
-import {NzTreeNodeOptions} from "ng-zorro-antd";
+import {NzTreeComponent, NzTreeNode, NzTreeNodeOptions} from "ng-zorro-antd";
 
 @Component({
   selector: 'app-sidebar-nav-custom',
@@ -11,6 +11,7 @@ import {NzTreeNodeOptions} from "ng-zorro-antd";
 export class SidebarNavigationComponent {
   @Input('navData') navData: NzTreeNodeOptions[] = [];
   @HostBinding('class.app-tree-node') public _nzTreeNodeClass = true;
+  @ViewChild('tree') nzTree: NzTreeComponent;
 
   constructor(private router?: Router) {
   }
@@ -20,8 +21,23 @@ export class SidebarNavigationComponent {
     if (e.node.children && e.node.children.length) {
       e.node.isExpanded = !e.node.isExpanded;
     } else {
+      this.nzTree.getCheckedNodeList().forEach(node => this.switchCheckedNodes(node));
+      [e.node, ...this.getNodeParents(e.node)].forEach(node => {
+        node.isChecked = true;
+      });
       let route = e.node.key.split("/").filter(r => r !== "");
       this.router.navigate(route);
     }
+  }
+
+  getNodeParents(node: NzTreeNode) {
+    if (node.parentNode) {
+      return [node.parentNode, ...this.getNodeParents(node.parentNode)];
+    } else return []
+  }
+
+  switchCheckedNodes(node: NzTreeNode) {
+    node.isChecked = false;
+    node.children.forEach(childNode => this.switchCheckedNodes(childNode));
   }
 }
