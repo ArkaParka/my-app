978041e1b4ea3e2ff01f9e2af5d35ada5a1a2256
Jever Osmanova <amidala274@gmail.com>
2020-07-15 19:26:20 +0300

    Перемещение загрузки компонентов в FormLoaderComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -58,10 +58,9 @@ import { MenuComponent } from './menu/menu.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpModule } from '@angular/http';
 //import {ContainerModule} from "../../shared/container-module/container.module";
-import { FormLoaderComponent } from './containers/form-loader/form-loader.component';
-import {MenuModule} from "./menu/menu.module";
 import {NzTreeModule} from "ng-zorro-antd";
 import {SidebarNavigationComponent} from "./containers/default-layout/sidebar-navigation/sidebar-navigation.component";
+import { FormLoaderModule } from './containers/form-loader/form-loader.module';
 
 @NgModule({
   imports: [
@@ -79,7 +78,7 @@ import {SidebarNavigationComponent} from "./containers/default-layout/sidebar-na
     ChartsModule,
     HttpClientModule,
     HttpModule,
-    MenuModule,
+    FormLoaderModule,
     NzTreeModule,
     NzIconModule
   ],
@@ -90,9 +89,7 @@ import {SidebarNavigationComponent} from "./containers/default-layout/sidebar-na
     P500Component,
     LoginComponent,
     RegisterComponent,
-    FormLoaderComponent,
     SidebarNavigationComponent
-    //MenuComponent,
   ],
   providers: [
     {
