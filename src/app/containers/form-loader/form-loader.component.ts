7d7c7f4f1c5e8dc23589cb4321d2ad3b1d40712a
Jever Osmanova <amidala274@gmail.com>
2020-07-16 12:51:15 +0300

    Компонеты выбираются в зависимости от типа

--- src/app/containers/form-loader/form-loader.component.ts
+++ src/app/containers/form-loader/form-loader.component.ts
@@ -1,5 +1,9 @@
 import {Component} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
+import { ModulePageConfiguration } from '../../models/ModulePageConfiguration.interface';
+import { Subject } from 'rxjs';
+import { switchMap, takeUntil } from 'rxjs/operators';
+import { DynamicMenuService } from '../../services/dynamic-menu.service';
 
 @Component({
   selector: 'app-form-loader',
@@ -7,14 +11,43 @@ import {ActivatedRoute} from "@angular/router";
   styleUrls: ['./form-loader.component.scss']
 })
 export class FormLoaderComponent {
+
   moduleKey: string;
   configPath: string;
+  public viewType;
+  public dataForComponent: {
+    moduleKey: string,
+    configPath: string,
+    pageConfiguration: ModulePageConfiguration
+  };
+  //public pageConfiguration: ModulePageConfiguration;
+  public isFormLoading: boolean = true;
 
+  destroy$: Subject<boolean> = new Subject<boolean>();
 
-  constructor(private route: ActivatedRoute) {
-    route.params.subscribe((params) => {
-      this.moduleKey = params['moduleKey'];
-      this.configPath = params['configPath'];
+  ngOnDestroy(): void {
+    this.destroy$.next(null);
+    this.destroy$.complete();
+  }
+
+  constructor(private dynamicMenuService: DynamicMenuService, private route: ActivatedRoute) {
+    route.params.pipe(
+      switchMap((params) => {
+        this.isFormLoading = false;
+        this.moduleKey = params['moduleKey'];
+        this.configPath = params['configPath'];
+        return this.dynamicMenuService.getModulePageConfiguration(this.moduleKey, this.configPath);
+      }),
+      takeUntil(this.destroy$)
+    )
+    .subscribe(resp =>{
+      console.log('ОТвет от сервера',  resp);
+      this.dataForComponent = {
+        moduleKey: this.moduleKey,
+        configPath: this.configPath,
+        pageConfiguration: resp
+      };
+      this.viewType = resp.viewConfig.type; 
     });
   }
 
