f770e48c61321bee93e766f4c17ac86fe3e306cc
andreykichatoff <andreykichatoff@gmail.com>
2021-02-16 14:08:26 +0300

    build fix

--- src/app/dynamic-page-view/dynamic-page-view.module.ts
+++ src/app/dynamic-page-view/dynamic-page-view.module.ts
@@ -18,7 +18,6 @@ import {PageViewComponent} from "./available-widgets/page-view-component/page-vi
 import {InputNumberComponent} from "./available-widgets/input-component/input-number.component";
 import {NgxMaskModule} from "ngx-mask";
 import {InputCheckboxComponent} from "./available-widgets/input-component/input-checkbox.component";
-import {DynamicIoModule, DynamicModule} from "ng-dynamic-component";
 
 const AvailableWidgets = [
   BlankComponent,
@@ -40,9 +39,7 @@ const AvailableWidgets = [
     NzTreeModule,
     NzIconModule,
     SharedModule,
-    NgxMaskModule.forRoot(),
-    DynamicModule,
-    DynamicIoModule
+    NgxMaskModule.forRoot()
   ],
   declarations: [
     DynamicPageMockComponent,
