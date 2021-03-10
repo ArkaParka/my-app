3608bc6b1ab056915dd359fb656285dc1f90e172
andreykichatoff <andreykichatoff@gmail.com>
2021-03-10 19:21:07 +0300

    Фикс цикличной перерисовки виджетов

--- src/app/dynamic-page-view/available-widgets/widget-lists/widget-list.component.ts
+++ src/app/dynamic-page-view/available-widgets/widget-lists/widget-list.component.ts
@@ -1,19 +1,17 @@
-import {Component, OnInit, ChangeDetectionStrategy, ViewEncapsulation, Input} from "@angular/core";
+import {Component, ChangeDetectionStrategy, ViewEncapsulation, Input, OnChanges, SimpleChanges} from "@angular/core";
 import {WidgetListItem, WidgetsFactoryService} from "../../dynamic-page-services/widgets-factory.service";
-import {IWidgetOptions} from "../../interfaces/IWidgetOptions";
 import {IDynamicPageViewConfig} from "../../../models/IDynamicPageViewConfig";
-import {IViewConfig} from "../../../models/ViewConfiguration.interface";
 import {IModulePageConfiguration} from "../../../models/IModulePageConfiguration";
 import {IAreasConfig} from "../../interfaces/IAreasConfig";
-import {map, switchMap} from "rxjs/operators";
-import {combineLatest} from "rxjs";
+import {DocumentBaseComponent} from "../../../containers/document-base.component";
+import {takeUntil} from "rxjs/operators";
 
 @Component({
   selector: "app-widget-list",
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
   template: `
-    <ng-container *ngFor="let widget of widgetService.widgetList(widgetAreas)|async">
+    <ng-container *ngFor="let widget of widgetList">
       <app-widget-wrapper
         [widgetComponent]="widget.widgetComponentType"
         [data]="widget.widgetData">
@@ -21,47 +19,26 @@ import {combineLatest} from "rxjs";
     </ng-container>
   `
 })
-export class WidgetListComponent implements OnInit {
-  private _pageConfig: IModulePageConfiguration;
-
+export class WidgetListComponent extends DocumentBaseComponent {
   @Input()
   public set pageConfig(options: IModulePageConfiguration | IDynamicPageViewConfig) {
-    // console.log('widget-list options', options)
     if ((options as IModulePageConfiguration).viewConfig) {
-      this._pageConfig = options as IModulePageConfiguration;
       this.widgetAreas = ((options as IModulePageConfiguration).viewConfig.config as IDynamicPageViewConfig).areasConfig;
     } else {
       this.widgetAreas = (options as IDynamicPageViewConfig).areasConfig;
-      // console.log("попались", options)
     }
 
-    // this.widgetService.widgetList(this.widgetAreas).pipe(
-    //   switchMap(widgets => {
-    //     let arr = [];
-    //     widgets.forEach(widget => arr.push(widget.widgetData.getData()))
-    //     return combineLatest(arr);
-    //   })
-    // ).subscribe(data=>{
-    //   console.log('widget-data', data)
-    // })
-    // console.log('widget-list areas', this.widgetAreas)
+    this.widgetService.widgetList(this.widgetAreas)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((widgets: WidgetListItem[]) => {
+        this.widgetList = widgets;
+      })
   };
 
-  public get pageConfig() {
-    return this._pageConfig;
-  }
-
   public widgetAreas: IAreasConfig[] = [];
-
-  public get widgets(): WidgetListItem[] {
-    return this._widgets;
-  }
-
-  private _widgets: WidgetListItem[];
+  public widgetList: WidgetListItem[] = [];
 
   constructor(public widgetService: WidgetsFactoryService) {
-  }
-
-  public ngOnInit(): void {
+    super();
   }
 }
